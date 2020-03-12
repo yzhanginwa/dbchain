f967c12931366fa1341e98804bb38215b91bd56d
Ethan Zhang <yzhang.wa@gmail.com>
2020-03-13 02:02:08 +0800

    to implement file uploading rest service

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/cosmos/cosmos-sdk v0.37.4
 	github.com/gorilla/mux v1.7.3
+	github.com/ipfs/go-ipfs-api v0.0.3
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
 	github.com/tendermint/go-amino v0.15.0
---
+++ x/cosmosapi/client/rest/upload.go
@@ -0,0 +1,41 @@
+package rest
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/cosmos/cosmos-sdk/client/context"
+    "github.com/yzhanginwa/cosmos-api/x/cosmosapi/internal/utils"
+    "github.com/cosmos/cosmos-sdk/types/rest"
+    "github.com/gorilla/mux"
+    shell "github.com/ipfs/go-ipfs-api"
+)
+
+func uploadFileHandler(cliCtx context.CLIContext) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+
+        _, err := utils.VerifyAccessCode(vars["accessToken"])
+        if err != nil {
+            rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to verify access token")
+            return
+        }
+
+        file, _, err := r.FormFile("file")
+
+        if err != nil {
+            rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+            return
+        }
+
+	sh := shell.NewShell("localhost:5001")
+	cid, err := sh.Add(file)
+	if err != nil {
+            rest.WriteErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("%s", err))
+            return
+	}
+
+        res := cid
+        rest.PostProcessResponse(w, cliCtx, res)
+    }
+}
+
