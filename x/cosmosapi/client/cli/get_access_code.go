cde1fba6545854c21a163f43ae41ca19768609ed
Ethan Zhang <yzhang.wa@gmail.com>
2020-03-26 04:14:50 +0800

    to use the new keyring to get the cli keys to sign access code

--- x/cosmosapi/client/cli/get_access_code.go
+++ x/cosmosapi/client/cli/get_access_code.go
@@ -4,45 +4,37 @@ import (
     "errors"
     "strconv"
     "time"
-    "bufio"
-
     "github.com/spf13/cobra"
     "github.com/spf13/viper"
+    sdk "github.com/cosmos/cosmos-sdk/types"
     "github.com/cosmos/cosmos-sdk/client/context"
     "github.com/cosmos/cosmos-sdk/codec"
     "github.com/cosmos/cosmos-sdk/client/flags"
-    "github.com/cosmos/cosmos-sdk/client/input"
     "github.com/cosmos/cosmos-sdk/client/keys"
+    cryptoKeys "github.com/cosmos/cosmos-sdk/crypto/keys"
     "github.com/mr-tron/base58"
     "github.com/yzhanginwa/cosmos-api/x/cosmosapi/internal/types"
     "github.com/tendermint/tendermint/crypto/secp256k1"
 )
 
 func GetCmdGetAccessCode(queryRoute string, cdc *codec.Codec) *cobra.Command {
-    return &cobra.Command{
+    resultCmd := &cobra.Command{
         Use: "access-code",
         Short: "show access code",
         Args: cobra.ExactArgs(1),
         RunE: func(cmd *cobra.Command, args []string) error {
             cliCtx := context.NewCLIContext().WithCodec(cdc)
 
-            name := args[0]
+            kb, err := cryptoKeys.NewKeyring(sdk.KeyringServiceName(), viper.GetString(flags.FlagKeyringBackend), viper.GetString(flags.FlagHome), cmd.InOrStdin())
+            if err != nil {
+                return err
+            }
 
-            kb, err := keys.NewKeyBaseFromDir(viper.GetString(flags.FlagHome))
-            if err != nil {
-                    return err
-            }
-    
-            buf := bufio.NewReader(cmd.InOrStdin())
-            passphrase, err := input.GetPassword("Enter passphrase to decrypt your key:", buf)
-            if err != nil {
-                    return err
-            }
-    
+            name := args[0]
             now := time.Now().UnixNano() / 1000000
             nowString := strconv.Itoa(int(now)) 
 
-            signature, pubKey, err := kb.Sign(name, passphrase, []byte(nowString))
+            signature, pubKey, err := kb.Sign(name, keys.DefaultKeyPass, []byte(nowString))
 
             if err != nil {
                 return err
@@ -56,5 +48,10 @@ func GetCmdGetAccessCode(queryRoute string, cdc *codec.Codec) *cobra.Command {
             }
         },
     }
-}
 
+    // borrowed from github.com/cosmos/cosmos-sdk/client/keys/root.go
+    resultCmd.PersistentFlags().String(flags.FlagKeyringBackend, flags.DefaultKeyringBackend, "Select keyring's backend (os|file|test)")
+    viper.BindPFlag(flags.FlagKeyringBackend, resultCmd.Flags().Lookup(flags.FlagKeyringBackend))
+
+    return resultCmd
+}
