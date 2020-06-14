4789ffd1324eda86dc068f7422d6b4fa515c8a7b
Ethan Zhang <yzhang.wa@gmail.com>
2020-06-15 02:06:07 +0800

    save code for now

--- x/dbchain/client/rest/oracle/tx.go
+++ x/dbchain/client/rest/oracle/tx.go
@@ -47,16 +47,76 @@ func buildAndSignAndBuildTxBytes(msg sdk.Msg, accNum uint64, seq uint64, privKey
     msgs := []sdk.Msg{msg}
     stdFee := authtypes.NewStdFee(200000, sdk.Coins{sdk.NewCoin("dbctoken", sdk.NewInt(1))})
 
-    stdSignMsg := authtypes.StdSignMsg{
-        ChainID:       "testnet",
-        AccountNumber: accNum,
-        Sequence:      seq,
-        Memo:          "",
-        Msgs:          msgs,
-        Fee:           stdFee,
-    }
+    stdSignMsgBytes := authtypes.StdSignBytes("testnet", accNum, seq, stdFee, msgs, "")
 
-    sig, err := privKey.Sign(stdSignMsg.Bytes())
+    //type StdSignDoc struct {
+    //        AccountNumber uint64            `json:"account_number" yaml:"account_number"`
+    //        ChainID       string            `json:"chain_id" yaml:"chain_id"`
+    //        Fee           json.RawMessage   `json:"fee" yaml:"fee"`
+    //        Memo          string            `json:"memo" yaml:"memo"`
+    //        Msgs          []json.RawMessage `json:"msgs" yaml:"msgs"`
+    //        Sequence      uint64            `json:"sequence" yaml:"sequence"`
+    //}
+
+    //func StdSignBytes(chainID string, accnum uint64, sequence uint64, fee StdFee, msgs []sdk.Msg, memo string) []byte {
+    //        msgsBytes := make([]json.RawMessage, 0, len(msgs))
+    //        for _, msg := range msgs {
+    //                msgsBytes = append(msgsBytes, json.RawMessage(msg.GetSignBytes()))
+    //        }
+    //        bz, err := ModuleCdc.MarshalJSON(StdSignDoc{
+    //                AccountNumber: accnum,
+    //                ChainID:       chainID,
+    //                Fee:           json.RawMessage(fee.Bytes()),
+    //                Memo:          memo,
+    //                Msgs:          msgsBytes,
+    //                Sequence:      sequence,
+    //        })
+    //        if err != nil {
+    //                panic(err)
+    //        }
+    //        return sdk.MustSortJSON(bz)
+    //}
+
+
+    //func (msg MsgInsertRow) GetSignBytes() []byte {
+    //    return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+    //}
+
+    //func MustMarshalJSON(x interface{}) []byte
+
+
+    //type StdFee struct {
+    //        Amount sdk.Coins `json:"amount" yaml:"amount"`
+    //        Gas    uint64    `json:"gas" yaml:"gas"`
+    //}
+    //
+    //// NewStdFee returns a new instance of StdFee
+    //func NewStdFee(gas uint64, amount sdk.Coins) StdFee {
+    //        return StdFee{
+    //                Amount: amount,
+    //                Gas:    gas,
+    //        }
+    //}
+    //
+    //// Bytes for signing later
+    //func (fee StdFee) Bytes() []byte {
+    //        // normalize. XXX
+    //        // this is a sign of something ugly
+    //        // (in the lcd_test, client side its null,
+    //        // server side its [])
+    //        if len(fee.Amount) == 0 {
+    //                fee.Amount = sdk.NewCoins()
+    //        }
+    //        bz, err := ModuleCdc.MarshalJSON(fee) // TODO
+    //        if err != nil {
+    //                panic(err)
+    //        }
+    //        return bz
+    //}
+
+
+    sig, err := privKey.Sign(stdSignMsgBytes)
+
     if err != nil {
         fmt.Println("Oracle: Failed to sign message!!!")
         return nil, err
@@ -68,6 +128,25 @@ func buildAndSignAndBuildTxBytes(msg sdk.Msg, accNum uint64, seq uint64, privKey
     }
 
     newStdTx := authtypes.NewStdTx(msgs, stdFee, []authtypes.StdSignature{stdSignature}, "")
+
+
+    //type StdTx struct {
+    //        Msgs       []sdk.Msg      `json:"msg" yaml:"msg"`
+    //        Fee        StdFee         `json:"fee" yaml:"fee"`
+    //        Signatures []StdSignature `json:"signatures" yaml:"signatures"`
+    //        Memo       string         `json:"memo" yaml:"memo"`
+    //}
+    //
+    //func NewStdTx(msgs []sdk.Msg, fee StdFee, sigs []StdSignature, memo string) StdTx {
+    //        return StdTx{
+    //                Msgs:       msgs,
+    //                Fee:        fee,
+    //                Signatures: sigs,
+    //                Memo:       memo,
+    //        }
+    //}
+
+
     txBytes, err := aminoCdc.MarshalBinaryLengthPrefixed(newStdTx)
     if err != nil {
         fmt.Println("Oracle: Failed to marshal StdTx!!!")
