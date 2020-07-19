6067204d6eea28f5eb0506fd12122b298f15890c
Ethan Zhang <yzhang.wa@gmail.com>
2020-07-20 00:07:45 +0800

    to save code (not compile)

--- x/dbchain/internal/super_script/eval/types.go
+++ x/dbchain/internal/super_script/eval/types.go
@@ -4,12 +4,20 @@ import (
 
 )
 
+type ReturnValue int
+const (
+    NIL ReturnValue = iota
+    FALSE
+    TRUE
+)
+
 type Program struct {
     CurrentAppId uint
     CurrentTable string
-    NewRecord map[string]string
-    Script string
-    SyntaxTree []Statement
+    NewRecord    map[string]string
+    Script       string
+    SyntaxTree   []Statement
+    Return       ReturnValue
 }
 
 func NewProgram(appId uint, tableName string, newRecord map[string]string, script string) *Program {
@@ -18,11 +26,20 @@ func NewProgram(appId uint, tableName string, newRecord map[string]string, scrip
         CurrentTable: tableName,
         NewRecord:    newRecord,
         Script:       script,
+        Return:       NIL
     }
 }
 
-func (p *Program) ParseScript() {
-
+func (p *Program) EvaluateScript(syntaxTree []Statement) bool {
+    for _, statement := range syntaxTree {
+        statement.Evaluate(p)
+        if p.Return == FALSE
+            return false
+        } else if p.Return == TRUE {
+            return true
+        }
+    }
+    return true
 }
 
 type Statement struct {
@@ -31,9 +48,41 @@ type Statement struct {
     Return string
 }
 
+func (s *Statement) Evaluate(p *Program) {
+    switch s.Return {
+    case "true":
+        p.Return = TRUE
+        return
+    case "false":
+        p.Return = FALSE
+        return
+    }
+
+    if s.Insert != nil {
+        (s.Insert).Evaluate(p)
+    }
+
+    if s.IfCondition != nil {
+        (s.IfCondition).Evaluate(p)
+    }
+}
+
 type IfCondition struct {
     Condition Condition
     Statements []Statement
+}
+
+func (ic *IfCondition) Evaluate(p *Program) {
+    if (ic.Condition).Evaluate(p) {
+        for _, statement := range ic.Statements{
+            statement.Evaluate(p)
+            if p.Return == FALSE
+                return false
+            } else if p.Return == TRUE {
+                return true
+            }
+        }
+    }
 }
 
 type Condition struct {
@@ -42,9 +91,17 @@ type Condition struct {
     Right interface{}              // single or multi value
 }
 
+func (c *Condition) Evaluate(p *Program) bool {
+    return true
+}
+
 type Insert struct {
     TableName string
     Value map[string]string
+}
+
+func (c *Insert) Evaluate(p *Program) {
+
 }
 
 type SingleValue struct {
@@ -52,8 +109,16 @@ type SingleValue struct {
     ThisExpr ThisExpression
 }
 
+func (s *SingleValue) Evaluate(p *Program) {
+
+}
+
 type ThisExpression struct {
     Items []interface{}
+}
+
+func (t *ThisExpression) Evaluate(p *Program) {
+
 }
 
 type MultiValue struct {
@@ -61,8 +126,16 @@ type MultiValue struct {
     ListLiteral ListLiteral
 }
 
+func (m *MultiValue) Evaluate(p *Program) {
+
+}
+
 type TableValue struct {
     Items []interface{}
+}
+
+func (t *TableValue) Evaluate(p *Program) {
+
 }
 
 type ListLiteral struct {
