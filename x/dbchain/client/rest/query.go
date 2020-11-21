980530190afa4c84fafc10ca0928d55d1a71a6cc
Ethan Zhang <yzhang.wa@gmail.com>
2020-11-21 11:49:51 +0800

    Implement restful service for query of can-insert-row

--- x/dbchain/client/rest/query.go
+++ x/dbchain/client/rest/query.go
@@ -3,7 +3,7 @@ package rest
 import (
     "fmt"
     "net/http"
-
+    "github.com/mr-tron/base58"
     "github.com/cosmos/cosmos-sdk/client/context"
 
     "github.com/cosmos/cosmos-sdk/types/rest"
@@ -122,6 +122,23 @@ func showCanAddColumnOptionHandler(cliCtx context.CLIContext, storeName string) 
     return func(w http.ResponseWriter, r *http.Request) {
         vars := mux.Vars(r)
         res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/can_add_column_option/%s/%s/%s/%s/%s", storeName, vars["accessToken"], vars["appCode"], vars["tableName"], vars["fieldName"], vars["option"]), nil)
+        if err != nil {
+            rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+            return
+        }
+        rest.PostProcessResponse(w, cliCtx, res)
+    }
+}
+
+func showCanInsertRowHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        rowFieldsJson, err := base58.Decode(vars["rowFieldsJsonBase58"])
+        if err != nil {
+            rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+            return
+        }
+        res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/can_insert_row/%s/%s/%s/%s/%s", storeName, vars["accessToken"], vars["appCode"], vars["tableName"], rowFieldsJson), nil)
         if err != nil {
             rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
             return
