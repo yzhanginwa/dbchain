5ca51673de66143197f292295b974f31f6f00e57
Ethan Zhang <yzhang.wa@gmail.com>
2020-11-23 16:05:11 +0800

    Implement reseful service show-app-users

--- x/dbchain/client/rest/query.go
+++ x/dbchain/client/rest/query.go
@@ -70,6 +70,19 @@ func showIsAppUserHandler(cliCtx context.CLIContext, storeName string) http.Hand
         rest.PostProcessResponse(w, cliCtx, res)
     }
 }
+
+func showAppUsersHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/app_users/%s/%s", storeName, vars["accessToken"], vars["appCode"]), nil)
+        if err != nil {
+            rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+            return
+        }
+        rest.PostProcessResponse(w, cliCtx, res)
+    }
+}
+
 func showTablesHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         vars := mux.Vars(r)
