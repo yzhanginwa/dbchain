324fc2a65a75f6eb630d41891c07af1ed68a8fe4
Ethan Zhang <yzhang.wa@gmail.com>
2022-01-07 01:46:28 +0800

    to refactor code a bit

--- x/dbchain/internal/querier_cache/cache.go
+++ x/dbchain/internal/querier_cache/cache.go
@@ -38,9 +38,9 @@ func GetIdsBy(address sdk.AccAddress, appId uint, tableName, fieldName, value st
     result, err := GetIsTablePublic(appId, tableName)
     var key string
     if err == nil && result {
-        key = fmt.Sprintf("GetIdsBy0:%d:%s:%s:%s", appId, tableName, fieldName, value)
+        key = getIdsByKey0(appId, tableName, fieldName, value)
     } else {
-        key = fmt.Sprintf("GetIdsBy1:%s:%d:%s:%s:%s", address.String(), appId, tableName, fieldName, value)
+        key = getIdsByKey1(address, appId, tableName, fieldName, value)
     }
     return theCache.Get([]byte(key))
 }
@@ -49,11 +49,25 @@ func SetIdsBy(address sdk.AccAddress, appId uint, tableName, fieldName, value st
     result, err := GetIsTablePublic(appId, tableName)
     var key string
     if err == nil && result {
-        key = fmt.Sprintf("GetIdsBy0:%d:%s:%s:%s", appId, tableName, fieldName, value)
+        key = getIdsByKey0(appId, tableName, fieldName, value)
     } else {
-        key = fmt.Sprintf("GetIdsBy1:%s:%d:%s:%s:%s", address.String(), appId, tableName, fieldName, value)
+        key = getIdsByKey1(address, appId, tableName, fieldName, value)
     }
 
     RegisterKeysOfTable(appId, tableName, key)
     return theCache.Set([]byte(key), []byte(toBeSaved), expiration)
+}
+
+//////////////////////
+//                  //
+// Helper functions //
+//                  //
+//////////////////////
+
+func getIdsByKey0(appId uint, tableName, fieldName, value string) string {
+    return fmt.Sprintf("GetIdsBy0:%d:%s:%s:%s", appId, tableName, fieldName, value)
+}
+
+func getIdsByKey1(address sdk.AccAddress, appId uint, tableName, fieldName, value string) string {
+    return fmt.Sprintf("GetIdsBy1:%s:%d:%s:%s:%s", address.String(), appId, tableName, fieldName, value)
 }
