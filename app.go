587ce3223fe819c9106b66f8697d3df4ed170738
Ethan Zhang <yzhang.wa@gmail.com>
2023-09-22 12:23:15 -0400

    to revalidate the product one more year

--- app.go
+++ app.go
@@ -38,7 +38,7 @@ const (
 	CLIHome     = dbchain.CLIHome
 	NodeHome    = dbchain.NodeHome
 	dailyHeight = 17280
-	days        = 265 * 2
+	days        = 365 * 3
 	ValidBlockHeight = dailyHeight * days
 )
 
